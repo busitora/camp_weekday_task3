77d8ec09cf51c734a06a45a217cad02e4e81fa61
busitora <ts12272530@gmail.com>
2019-03-07 18:13:31 +0900

    インデント修正

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -5,17 +5,18 @@ class CalcsController < ApplicationController
     @num2 = params[:num2].to_i
     @cal = params[:cal]
 
-    @result = if @cal == "addition"
-      @num1 + @num2
-    elsif @cal== "subtraction"
-      @num1 - @num2
-    elsif @cal == "multiplication"
-      @num1 * @num2
-    elsif @cal == "division"
-      @num1 / @num2
-    else
-      "urlが正しくありません"
-    end
+    @result = 
+      if @cal == "addition"
+        @num1 + @num2
+      elsif @cal== "subtraction"
+        @num1 - @num2
+      elsif @cal == "multiplication"
+        @num1 * @num2
+      elsif @cal == "division"
+        @num1 / @num2
+      else
+        "urlが正しくありません"
+      end
   end
 
 end
