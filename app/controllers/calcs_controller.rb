e297825a5fb000a8f527ebc2d04658109191242e
busitora <ts12272530@gmail.com>
2019-03-07 12:20:39 +0900

    helperを使った簡略化

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -1,25 +1,10 @@
 class CalcsController < ApplicationController
-
+  include CalcsHelper
   def show
     @num1 = params[:num1].to_i
     @num2 = params[:num2].to_i
     @cal = params[:cal]
-
-    if
-      @cal == "addition"
-      @result = @num1 + @num2
-    elsif
-      @cal== "subtraction"
-      @result = @num1 - @num2
-    elsif
-      @cal == "multiplication"
-      @result = @num1 * @num2
-    elsif
-      @cal == "division"
-      @result = @num1 / @num2
-    else
-      @result = "URLが正しくありません"
-    end
+    result
   end
 
 end
--- app/helpers/calcs_helper.rb
+++ app/helpers/calcs_helper.rb
@@ -1,2 +1,15 @@
 module CalcsHelper
+  def result
+    @result = if @cal == "addition"
+      @num1 + @num2
+    elsif @cal == "subtraction"
+      @num1 - @num2
+    elsif @cal == "multiplication"
+      @num1 * @num2
+    elsif @cal == "division"
+      @num1 / @num2
+    else 
+      "URLが正しくありません"
+    end
+  end
 end
